1ad94bbddd60a1ddaa2f9fcd0a95fac80f9d9d8c
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2024-01-09 23:18:20 +0500

    feature: new component select in basic variant logic.

---
+++ src/components/ui/Select/Select.tsx
@@ -0,0 +1,46 @@
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
+
+import * as SelectRadix from '@radix-ui/react-select'
+
+type Props = {
+  className?: string
+  disabled?: boolean
+  label?: string
+  placeholder?: string
+  value?: string
+} & ComponentPropsWithoutRef<typeof SelectRadix.Root>
+export const Select = forwardRef<ElementRef<typeof SelectRadix.Root>, Props>(
+  (props: Props, ref) => {
+    const {
+      children,
+      disabled,
+      label,
+      onOpenChange,
+      onValueChange,
+      open,
+      placeholder,
+      value,
+      ...rest
+    } = props
+
+    return (
+      <SelectRadix.Root
+        disabled={disabled}
+        onValueChange={onValueChange}
+        open={open}
+        value={value}
+        {...rest}
+      >
+        <SelectRadix.Trigger ref={ref}>
+          <SelectRadix.Value placeholder={placeholder} />
+          <SelectRadix.Icon></SelectRadix.Icon>
+        </SelectRadix.Trigger>
+        <SelectRadix.Portal>
+          <SelectRadix.Content ref={ref}>
+            <SelectRadix.Viewport>{children}</SelectRadix.Viewport>
+          </SelectRadix.Content>
+        </SelectRadix.Portal>
+      </SelectRadix.Root>
+    )
+  }
+)
