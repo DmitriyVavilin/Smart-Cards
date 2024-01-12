7cf8363e59cff73826d07c76174cdfc86b9bbc2c
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2024-01-13 01:59:41 +0500

    fix: refactoring styles Input

--- src/assets/CloseEyes.tsx
+++ src/assets/CloseEyes.tsx
@@ -9,7 +9,7 @@ const CloseEyes = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
     xmlns={'http://www.w3.org/2000/svg'}
     {...props}
   >
-    <g clipPath={'url(#a)'} fill={'#000'}>
+    <g clipPath={'url(#a)'} fill={'#fff'}>
       <path
         d={
           'M4.71 3.29a1 1 0 1 0-1.42 1.42l5.63 5.63a3.5 3.5 0 0 0 4.74 4.74l5.63 5.63a1 1 0 1 0 1.42-1.42l-16-16ZM12 13.5a1.5 1.5 0 0 1-1.5-1.5v-.07l1.56 1.56-.06.01Z'
