1ebf37332082790a23a337e6283e8a5b04940849
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2024-03-08 13:08:05 +0500

    fix: refactoring component login-form

--- src/components/auth/login-form/login-form.tsx
+++ src/components/auth/login-form/login-form.tsx
@@ -1,56 +1,28 @@
-import { useForm } from 'react-hook-form'
-
+import { loginFormValues, useLoginForm } from '@/components/auth/login-form/useLoginForm'
 import { Button } from '@/components/ui/Button'
-import { ControlledCheckBox } from '@/components/ui/Controlled-Checkbox/ControlledCheckBox'
-import { Input } from '@/components/ui/Input'
+import { ControlledCheckBox } from '@/components/ui/controlled/Controlled-Checkbox/ControlledCheckBox'
+import { ControlledInput } from '@/components/ui/controlled/controlled-Input/controlledInput'
 import { DevTool } from '@hookform/devtools'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { z } from 'zod'
 
-const loginSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(3).max(30),
-  rememberMe: z.boolean().optional(),
-})
+import s from './login-form.module.scss'
 
-type FormValues = z.infer<typeof loginSchema>
 type Props = {
-  onSubmit: (values: FormValues) => void
+  onSubmit: (values: loginFormValues) => void
 }
 
 export const LoginForm = ({ onSubmit }: Props) => {
-  const {
-    control,
-    formState: { errors },
-    handleSubmit,
-    register,
-  } = useForm<FormValues>({
-    defaultValues: {
-      email: '',
-      password: '',
-      rememberMe: false,
-    },
-    resolver: zodResolver(loginSchema),
-  })
+  const { control, handleSubmit } = useLoginForm()
 
   return (
     <>
       <DevTool control={control} />
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Input
-          error={errors.email?.message}
-          type={'email'}
-          {...register('email')}
-          label={'email'}
-        />
-        <Input
-          error={errors.password?.message}
-          type={'password'}
-          {...register('password')}
-          label={'password'}
-        />
-        <ControlledCheckBox control={control} label={'remember me'} name={'rememberMe'} />
-        <Button type={'submit'}>Submit</Button>
+        <ControlledInput control={control} label={'Email'} name={'email'} type={'email'} />
+        <ControlledInput control={control} label={'Password'} name={'password'} type={'password'} />
+        <ControlledCheckBox control={control} label={'Remember me'} name={'rememberMe'} />
+        <Button className={s.btnSubmit} type={'submit'}>
+          Submit
+        </Button>
       </form>
     </>
   )
